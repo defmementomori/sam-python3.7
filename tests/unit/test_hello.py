72d3fd36345c754c3418a2ef167bcedb6bd9a6c6
defmementomori <defmementomori@gmail.com>
2021-12-16 17:32:22 +0900

    add sample test

--- tests/unit/test_hello.py
+++ tests/unit/test_hello.py
@@ -5,6 +5,11 @@ import re
 import os
 from functions.hello_world import app as app_hello
 
+
 def test_echo():
     print("pytest start")
     app_hello.lambda_handler("","")
+
+def test_function_sample():
+    assert app_hello.function_sample(2,3)==5
+
