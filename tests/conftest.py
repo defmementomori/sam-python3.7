1f46fc34ed7b47397493402abfc53c07e10507f2
defmementomori <defmementomori@gmail.com>
2021-12-16 08:35:25 +0000

    add sample test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ import json
 import boto3
 
 sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../functions/"))
-print('******************confitest global******************')
 
 def set_environment_variable(json_filepath):
     """Jsonを読み込み環境変数にセットする."""
@@ -15,5 +14,4 @@ def set_environment_variable(json_filepath):
 
 #環境変数をセット
 set_environment_variable('./tests/variable.local.json')
-
 
